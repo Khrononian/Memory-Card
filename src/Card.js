8732a0d572289131e5f1523ca48112d071db50b7
Khrononian <Alv.Barnes@gmail.com>
2023-05-27 19:39:14 -0400

    Testing code

--- src/Card.js
+++ src/Card.js
@@ -71,6 +71,7 @@ const Card = () => {
 
     useEffect(() => {
         if (score.length >= bestScore) setBestScore(prevBestScore => prevBestScore = score.length)
+        console.log('Test', score)
         if (score.length === 12) setScore(prevScore => score.length = prevScore.splice(0, 11)) 
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [score])
